050ce956dac6775a371899f175c2e25e63edf7f5
Hindola-sta <147393885+Hindola-sta@users.noreply.github.com>
2023-11-11 21:31:48 +0530

    Update main.js
    reemove

--- main.js
+++ main.js
@@ -70,14 +70,7 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }    
-window.addEventListener("load",()=>{
-  const loader=document.querySelector(".loader");
 
-  loader.classList.add("loader-hidden");
-  loader.addEventListener("transitioned",()=>{
-    document.body.removeChild("loader");
-  })
-})
 
 
 
